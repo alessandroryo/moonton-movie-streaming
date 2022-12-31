629cdfbb5f8a41ca84c0f86432ae902eb0133224
Alessandro Ryo <alessandroryo@gmail.com>
2022-12-31 22:00:38 +0100

    feat: Slicing Sign In and Sign Up

--- resources/js/Pages/Auth/ConfirmPassword.jsx
+++ resources/js/Pages/Auth/ConfirmPassword.jsx
@@ -1,9 +1,9 @@
 import { useEffect } from 'react';
 import GuestLayout from '@/Layouts/GuestLayout';
 import InputError from '@/Components/InputError';
-import InputLabel from '@/Components/InputLabel';
-import PrimaryButton from '@/Components/PrimaryButton';
-import TextInput from '@/Components/TextInput';
+import InputLabel from '@/Components/Label';
+import PrimaryButton from '@/Components/Button';
+import TextInput from '@/Components/Input';
 import { Head, useForm } from '@inertiajs/inertia-react';
 
 export default function ConfirmPassword() {
